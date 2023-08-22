d800dc0c92bd99e18dc9937efbf2dedd28f1ad57
Fang_Woei <fangwoei22f@forwardschool.edu.my>
2023-08-22 14:48:35 +0800

    .

--- routes/tvshow.js
+++ routes/tvshow.js
@@ -1,28 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
+// import model into router
 const tvshow = require("../models/tvshow");
 
+/* list all the tvshows */
 router.get("/", async (req, res) => {
-  const { premiere_year, seasons, genre } = req.query;
-let filter = {};
-if (premiere_year || seasons || genre) {
-  if (premiere_year) {
-    filter.premiere_year = { $gt: premiere_year };
+  const { premiere_year, genre, rating } = req.query;
+  let filter = {};
+  if (genre || rating || premiere_year) {
+    if (genre) {
+      filter.genre = { $in: genre.split(",") }; // { genre: { $in: genre } }
+    }
+    if (rating) {
+      filter.rating = { $gt: rating }; // { rating: { $gt: rating } }
+    }
+    if (premiere_year) {
+      filter.premiere_year = { $gt: premiere_year }; // { premiere_year: { $gt: release_year } }
+    }
   }
-  if (seasons) {
-    filter.seasons = { $gt: seasons };
-  }
-  if (genre) {
-    filter.genre = genre;
-  }
-}
-  res.send(await tvshow.find(filter));
+  const list = await tvshow.find(filter);
+  res.send(list);
 });
 
+/* get specific tvshow by id */
 router.get("/:id", async (req, res) => {
-  const tvdata = await tvshow.findOne({ _id: req.params.id });
-  res.send(tvdata);
+  const data = await tvshow.findOne({ _id: req.params.id });
+  res.send(data);
 });
 
 module.exports = router;
